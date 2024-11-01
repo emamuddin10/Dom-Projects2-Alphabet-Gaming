85840480d10066a168b518ddce338a16bc8c4809
mr coder <emamskingdom@gmail.com>
2024-11-02 03:47:05 +0600

    added click section

---
+++ script/alpha-clash.js
@@ -0,0 +1,13 @@
+// function play(){
+//     const homeSection = document.getElementById('home-section')
+//     homeSection.classList.add('hidden')
+
+//     const playGroundSection = document.getElementById('play-ground-section')
+//         playGroundSection.classList.remove('hidden')
+// }
+
+
+function play(){
+    hideElementById('home-section')
+    showElementById('play-ground-section')
+}
