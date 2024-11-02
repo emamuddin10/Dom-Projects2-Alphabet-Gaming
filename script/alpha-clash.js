711d20dd23208a3e32b913bd7870d4b62272e399
mr coder <emamskingdom@gmail.com>
2024-11-03 00:11:59 +0600

    added alphabet function

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -5,9 +5,24 @@
 //     const playGroundSection = document.getElementById('play-ground-section')
 //         playGroundSection.classList.remove('hidden')
 // }
+function continueGame(){
+    //genarate a random alphabet
+    const alphabet = getARandomAlphabet()
+    console.log('your random alphabet', alphabet)
+
+    //set randomly genareted alphabet
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet
+    
+    // set background color
+    setBackgroundColorById(alphabet)
+
+}
 
 
 function play(){
     hideElementById('home-section')
     showElementById('play-ground-section')
+    continueGame()
+    
 }
